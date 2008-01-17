06ca65d7ab5d84874c111097c6142ad9bea2dcb2
faivrem <faivrem@096587a4-8a43-0410-95db-157a40d67bfe>
2008-01-17 15:27:08 +0000

    some camera work

--- trunk/camera.rb
+++ trunk/camera.rb
@@ -5,30 +5,25 @@ class Camera
   attr_accessor :pos, :view, :rot
   
   def initialize
-    @pos  = MVector.new(0,10,100) # {:x=>0,:y=>10, :z=>100}
-    @view = MVector.new(0,0,0) # {:x=>0,:y=>0, :z=>0}
-    @rot  = MVector.new(0,1,0) # {:x=>0,:y=>1, :z=>0}
+    @pos  = MVector.new(0,10,100)
+    @view = MVector.new(0,0,0)
+    @rot  = MVector.new(0,1,0)
   end
   
   
   # the cam moves forward (following its view) with the z parameter
   # the view is moved with parameters x and y  
   def move(x,y,z)
-    # rotate (x,y)/z
+    # rotate (joy1x,joy1y)
     v = @view-@pos # view vector
-    puts "  #{v}" if CONFIG[:log][:camera]
-    v.rotate(x,y,0)
-    puts "=>#{v}" if CONFIG[:log][:camera]
+    #puts "  #{v}" if CONFIG[:log][:camera]
+    v.rotate(x/1000,y/1000,0)
+    #puts "=>#{v}" if CONFIG[:log][:camera]
     @view = @pos+v
-    # translate (z)
-    
-    #@camxa += @joy1x / 100.0
-    #@camya += @joy1y / 100.0
-    #@camz  += @joy1z / 100.0
-    
-    #pos.x += x / 100.0
-    #pos.y += y / 100.0
-    #pos.z += z / 100.0
+
+    # translate (joy2y)
+    v = @view-@pos # view vector
+    @pos = @pos+v*(z/100)
   end
   
 end
