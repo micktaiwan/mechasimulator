ef976fee3545a4d4c15d842357ca72362e7d136e
faivrem <faivrem@096587a4-8a43-0410-95db-157a40d67bfe>
2008-01-17 16:32:22 +0000

    cam work

--- camera.rb
+++ camera.rb
@@ -5,8 +5,8 @@ class Camera
   attr_accessor :pos, :view, :rot
   
   def initialize
-    @pos  = MVector.new(0,10,100)
-    @view = MVector.new(0,0,0)
+    @pos  = MVector.new(584,263,510)
+    @view = MVector.new(769,347,686)
     @rot  = MVector.new(0,1,0)
   end
   
@@ -14,16 +14,15 @@ class Camera
   # the cam moves forward (following its view) with the z parameter
   # the view is moved with parameters x and y  
   def move(x,y,z)
+    puts "cam: pos: #{pos} view: #{view} rot: #{rot}" if (x!=0 or y!=0 or z!=0) and CONFIG[:log][:camera]
     # rotate (joy1x,joy1y)
     v = @view-@pos # view vector
-    #puts "  #{v}" if CONFIG[:log][:camera]
     v.rotate(x/1000,y/1000,0)
-    #puts "=>#{v}" if CONFIG[:log][:camera]
     @view = @pos+v
-
+    
     # translate (joy2y)
-    v = @view-@pos # view vector
-    @pos = @pos+v*(z/100)
+    #v = @view-@pos # view vector
+    @pos = @pos+@view*(z/400)
   end
   
 end
