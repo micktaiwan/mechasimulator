08c43a4fccef2ee3cddf2e19be0415c289577a80
faivrem <faivrem@096587a4-8a43-0410-95db-157a40d67bfe>
2009-01-08 15:16:15 +0000

    position option for follow

--- objects.rb
+++ objects.rb
@@ -1,15 +1,25 @@
+# Example 3 : Piston
+
 object
-  20.times do |i|
-    p(i*0.1,0, 3)
-    string :last_two # do nothing if only one last element
-  end
-  fix   :first
-  motor :last, :first, [0,0,1], 1
+  c = p(0,0,1)
+  fix c
+  f = p(1,0,1)
+  p(0,0,3)
+  
+  fix from = p(-5,-4,1)
+
+  string 0,1
+  string 1,2
+  boundary 2, :x, :>, -0.01
+  boundary 2, :x, :<,  0.01
+  boundary 2, :y, :>, -0.01
+  boundary 2, :y, :<,  0.01
+
+  motor 1, c, [0,1,0], -9.82
 end_object
 
 gravity :all
+follow f, {:position=>from}
 
-follow :last, {:distance=>4, :side=>1}
-# à cause du frustrum qui est à 1.5, on ne voit pas les objects si on est à moins de 1.5 de eux
-# donc si tu veux voir le point que tu suit,il faut le suivre à > 1.5 de distance
+
 
