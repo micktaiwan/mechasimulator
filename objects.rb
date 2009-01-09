e554dd5a409c6ecea1b90198e2fb3407868dbcb3
faivrem <faivrem@096587a4-8a43-0410-95db-157a40d67bfe>
2009-01-09 00:36:25 +0000

    finished gravit, with reverse option

--- objects.rb
+++ objects.rb
@@ -1,7 +1,8 @@
+
 object
   a = p(0,0,1)
   uni :last, [0, 0, 0.01]
-  p(1,0,1)
-  gravit :last, a, {:factor=>20, :inverse=>true}
+  p(2,0,1)
+  gravit :last, a, {:factor=>0.1, :reverse=>nil}
 end_object
 
